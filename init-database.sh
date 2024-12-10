a108b13232a3c1ba684a1dec481c0a9d0add216d
Your Name <yourname@example.com>
2024-12-10 13:49:23 +0100

    .

--- init-database.sh
+++ init-database.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-# Connect to the default database 'postgres' and create the required databases
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    DROP DATABASE IF EXISTS news_app_db;
     CREATE DATABASE news_app_db;
-    CREATE DATABASE news_app_test;
 EOSQL
